931aa8e4e49694ce7c44bed7b33852ac13bc00d5
NickoLevin <niklev.02@mail.ru>
2022-02-05 21:46:23 +0500

    finish

--- pages/about.js
+++ pages/about.js
@@ -2,35 +2,49 @@ import React from "react";
 import MainLayout from "../components/MainLayout";
 import { Typography } from "@mui/material";
 import Loader from '../components/Loader'
-import { Container } from "@mui/material";
+import instance from '../API';
+import { useState, useEffect } from "react";
+
+
 
 function about() {
+
+  const [about, setAbout] = useState();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+      instance.get('/api/brom/about_page').then((res) => {
+          if (res.status === 200) {
+              setAbout(res.data);
+              setLoading(false);
+          }
+      });
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+}
+
+
   return (
     <MainLayout>
       <div style={{ marginTop: "100px" }}>
         <Typography variant="h2" align="center" gutterBottom>
           About us!
         </Typography>
-          <Container maxWidth="sm" style={{ marginTop: "50px", backgroundImage: './home_page_img.jpg'}} >
-            <Typography variant="h6" align="center" gutterBottom>
-            Таким образом реализация намеченных плановых заданий позволяет оценить значение новых предложений.
-             Таким образом новая модель организационной деятельности способствует подготовки и реализации систем массового участия. 
-             С другой стороны укрепление и развитие структуры обеспечивает участие в формировании систем массового участия.
-            </Typography>
-            <Typography variant="h6" align="center" gutterBottom>
-            Равным образом постоянный количественный рост и сфера нашей активности играет важную роль в формировании системы обучения кадров,
-             соответствует насущным потребностям. Повседневная практика показывает, 
-             что реализация намеченных плановых заданий в значительной степени обуславливает создание модели развития.
-            </Typography>
-            <Typography variant="h6" align="center" gutterBottom>
-            Товарищи! сложившаяся структура организации представляет собой интересный эксперимент проверки направлений прогрессивного развития.
-             Разнообразный и богатый опыт консультация с широким активом обеспечивает широкому кругу. 
-             Таким образом новая модель организационной деятельности способствует подготовки и реализации систем массового участия.
-            </Typography>
-
-
-          </Container>
-  
+        <Typography
+            component="div"
+            variant="h6"
+            dangerouslySetInnerHTML={{ __html: about.text }}
+        />
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
       </div>
     </MainLayout>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Typography } from '@mui/material';
+import Loader from '../components/Loader';
+import instance from '../API';
 import MainLayout from '../components/MainLayout';
 import CarouselMy from '../components/CarouselMy';
-import Loader from '../components/Loader';
-import Widget from '../components/Widget';
-
 
 function index() {
+  const [text, setText] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    instance.get('/api/brom/home_page').then((res) => {
+        if (res.status === 200) {
+            setText(res.data);
+            setLoading(false);
+        }
+    });
+}, []);
+
+if (loading) {
+  return <Loader />;
+}
 
   return (
   <MainLayout>
    <div style={{ marginTop: "100px" }}>
-        <Typography variant="h2" align="center" gutterBottom>
-          Welcome to brom.ru
-        </Typography>
-        <Typography variant="body1" align="center" gutterBottom>
-        Значимость этих проблем настолько очевидна, что дальнейшее развитие различных форм деятельности
-         обеспечивает широкому кругу (специалистов) участие в формировании новых предложений. 
-         Повседневная практика показывает, что реализация намеченных плановых заданий в значительной степени обуславливает создание модели развития.
-        </Typography>
-        <Typography variant="body1" align="center" gutterBottom>
-        Значимость этих проблем настолько очевидна, что дальнейшее развитие различных форм деятельности обеспечивает широкому кругу 
-        (специалистов) участие в формировании новых предложений. Идейные соображения высшего порядка, а также дальнейшее развитие различных форм 
-        деятельности позволяет оценить значение новых предложений.
-        </Typography>
-        <Typography variant="body1" align="center" gutterBottom>
-        С другой стороны постоянное информационно-пропагандистское обеспечение нашей 
-        деятельности обеспечивает широкому кругу (специалистов) участие в формировании позиций, занимаемых участниками в отношении поставленных задач. 
-        С другой стороны рамки и место обучения кадров способствует подготовки и реализации модели развития.
-
-        </Typography>
+   <Typography
+                component="div"
+                variant="h6"
+                dangerouslySetInnerHTML={{ __html: text.text }}
+            />
+            <br/>
         <CarouselMy/>
        
         </div>
